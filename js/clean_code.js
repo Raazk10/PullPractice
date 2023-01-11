b5406023e872f4cb1103be8906d8c1fba538d1b1
Raazk10 <rakioraag10@gmail.com>
2023-01-11 11:21:26 +0100

    second commit

---
+++ js/clean_code.js
@@ -0,0 +1,20 @@
+/*
+	CLEAN CODE
+
+	- don't be cute
+	- methods should have verbs
+	- use searchable names
+	- use pronouncable names
+	- small
+	- do one thing (unngå side-effects)
+	- one word per concept (f.eks. fetch/get/return, wrapper/container)
+*/
+
+// query selector
+const testButton = document.querySelector(".header__menu-button");
+
+// event listner
+
+testButton.addEventListener("click", handleTestButtonClick);
+
+// event handler
